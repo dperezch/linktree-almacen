6cc8564f775f55580e141beb2e7c7c817230e957
dperezch <davarmando@gmail.com>
2023-11-30 14:15:50 -0300

    welcome.vue, enlaces.vue, terminado.

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <main class="bg-no-repeat bg-cover bg-center bg-[url('/shiny.svg')] w-screen h-screen">
     <section>
       <Logo/>
+      <Welcome />
       <Enlaces />
     </section>
   </main>
 </template>
 
 <script setup lang="ts">
+import Welcome from "./components/Welcome.vue"
 import Enlaces from "./components/Enlances.vue"
 import Logo from "./components/Logo.vue"
 
---
+++ src/components/Welcome.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="text-center text-slate-50 p-4">
+        <h1 class="text-xl font-semibold my-1">Bienvenido a nuestras redes sociales!</h1>
+        <h1 class="font-thin">Pincha en el enlace que necesites para enviar archivos, mensajes, o correos electrónicos:</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
