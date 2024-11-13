851ed99236efa9da3702a57dcf8f586c3399863b
dperezch <davarmando@gmail.com>
2024-11-13 18:40:52 -0300

    background repeat

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <main class="bg-no-repeat bg-cover bg-center bg-[url('/shiny.svg')] w-screen h-screen">
+  <main
+    class="bg-repeat bg-cover bg-center bg-[url('/shiny.svg')] w-screen h-screen"
+  >
     <section>
-      <Logo/>
+      <Logo />
       <Welcome />
       <Enlaces />
     </section>
@@ -9,12 +11,9 @@
 </template>
 
 <script setup lang="ts">
-import Welcome from "./components/Welcome.vue"
-import Enlaces from "./components/Enlances.vue"
-import Logo from "./components/Logo.vue"
-
+import Welcome from "./components/Welcome.vue";
+import Enlaces from "./components/Enlances.vue";
+import Logo from "./components/Logo.vue";
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
